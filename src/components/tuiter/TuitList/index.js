711f7fad26a15d97d2c493a6743dfcdd0942b868
Nicholas Contreras <nicholasc2000@yahoo.com>
2022-04-15 12:56:34 -0400

    include dispatch as dependency to useEffect

--- src/components/tuiter/TuitList/index.js
+++ src/components/tuiter/TuitList/index.js
@@ -6,7 +6,7 @@ import {findAllTuits} from "../../../actions/tuits-actions";
 const TuitList = () => {
     const tuits = useSelector(state => state.tuits);
     const dispatch = useDispatch();
-    useEffect(() => findAllTuits(dispatch), [tuits]);
+    useEffect(() => findAllTuits(dispatch), [dispatch, tuits]);
 
     return (
         <>
