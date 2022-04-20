bd64795deae3952baca01ca8f46e6be4f2ff26c6
Nicholas Contreras <nicholasc2000@yahoo.com>
2022-04-20 14:48:35 -0400

    async fun

--- src/vendors/BlueprintsAPI.js
+++ src/vendors/BlueprintsAPI.js
@@ -1,13 +1,12 @@
-import Blueprint from "../components/BlueprintGrid/Blueprint";
-
 const axios = require('axios');
 
 const URL_BASE = "https://www.factorio.school/api/";
 
-const get_top_blueprints = async () => {
+const get_top_blueprints = () => {
     const url = URL_BASE + "blueprintSummaries/top/page/1?title=";
-    const response = await axios.get(url);
-    console.log(response);
+    axios.get(url).then((response) => {
+        console.log(response);
+    });
 };
 
 const BlueprintsAPI = {get_top_blueprints};
