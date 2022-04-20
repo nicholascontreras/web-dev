86077d0b6b6b55bb4c202f050dfc16d9b7121a00
Nicholas Contreras <nicholasc2000@yahoo.com>
2022-04-20 15:42:28 -0400

    update alt text

--- src/components/BlueprintGrid/Blueprint.js
+++ src/components/BlueprintGrid/Blueprint.js
@@ -3,7 +3,7 @@ import React from "react";
 const Blueprint = ({name, imageURL}) => {
     return (
         <div className="card">
-            <img className={"card-img-top"} src={imageURL} alt={"Blueprint preview image"}/>
+            <img className={"card-img-top"} src={imageURL} alt={"Blueprint preview"}/>
             <div className="card-body">
                 <h5 className="card-title">{name}</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
